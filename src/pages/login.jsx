6e5bd26436b653831bef000be993a79e087d6bfd
Kerosz <andrei_chirila@outlook.com>
2021-03-05 15:26:00 +0200

    setup formik and added images

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -11,8 +11,13 @@ export default function Login() {
   }, []);
 
   return (
-    <div className="container flex mx-auto max-w-screen-md items-center">
-      login
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>I will be the form</p>
+      </div>
     </div>
   );
 }
