1ed59a16284cae8a0a6902476fc84934aff50b25
Kerosz <andrei_chirila@outlook.com>
2021-03-12 16:41:19 +0200

    feature: added not-found page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,18 +1,26 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
+import Header from '../components/header';
 
 export default function NotFound() {
-  const location = useLocation();
-
   useEffect(() => {
     document.title = `Not Found - Instagram`;
   }, []);
 
   return (
     <div className="bg-gray-background">
+      <Header />
       <div className="mx-auto max-w-screen-lg">
-        <p className="text-center text-2xl">
-          No page found at this path <code>{location.pathname}</code>
+        <p className="text-center text-2xl font-semibold text-gray-800">
+          Sorry, this page isn't available.
+        </p>
+        <p className="text-center mt-10">
+          The link you followed may be broken, or the page may have been
+          removed.{' '}
+          <Link to={ROUTES.DASHBOARD} className="text-blue-medium">
+            Go back to Instagram.
+          </Link>
         </p>
       </div>
     </div>
